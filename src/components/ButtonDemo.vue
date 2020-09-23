cf2d375524730771e2abc78d680a833bfaae2ec6
zhangbin_yq <zhangbin_yq@kedacom.com>
2020-09-23 22:45:30 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,44 +1,58 @@
 <template>
 	<section>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
-		<p>Button 文档</p>
+		<div>Button 示例</div>
+		<h1>示例1</h1>
+		<Button @click="onClick"
+						@focus="onClick"
+						size="mini"
+						disabled
+						@mouseover="onClick">按钮
+		</Button>
+		<ul class="note">
+			<li>vue3 属性绑定</li>
+			<li>默认所有属性都绑定到根元素</li>
+			<li>使用inheritAttrs:false可以取消默认绑定</li>
+			<li>使用$attrs或者context.attrs获取所有属性</li>
+			<li>使用const{size, ...rest} = context.attrs将属性分开</li>
+		</ul>
 	</section>
 </template>
 
 <script>
+import Button from '../lib/Button.vue';
+
 export default {
-	name: 'Button'
+	name: 'ButtonDemo',
+	components: {
+		Button
+	},
+	setup() {
+		const onClick = () => {
+			console.log('xxx');
+		};
+
+		return {
+			onClick
+		};
+	}
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+section {
+	.note {
+		text-align: left;
 
+		> li {
+			margin: 4px auto;
+
+			&:first-child {
+				font-weight: bold;
+				list-style: none;
+				margin-left: -15px;
+			}
+		}
+
+	}
+}
 </style>
---
+++ src/lib/Button.vue
@@ -0,0 +1,35 @@
+/**
+*Created by bin on 2020/9/23
+*/
+<template>
+	<div>
+		<button v-bind="$attrs">
+			<slot/>
+		</button>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'Button',
+	inheritAttrs: false,
+	// 1.
+	props: {
+		disabled: Boolean
+	},
+	setup(props, context){
+		// 在1.声明才能拿到值, attrs不用先声明
+		// props不包含事件， attrs包含
+		// props 没有声明的属性， 会跑到attrs里面
+		// props支持string以外的类型， attrs只有string类型
+		/*console.log({...props})
+		console.log({...context.attrs})*/
+	}
+};
+</script>
+
+<style lang='scss' scoped>
+div {
+	border: 1px solid red;
+}
+</style>
