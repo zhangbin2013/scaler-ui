1bb410aee383e87224d91a8a9204f4b4b6a2fe14
zhangbin_yq <zhangbin_yq@kedacom.com>
2020-10-02 14:50:26 +0800

    tabs 检查子组件类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,29 @@
 <template>
 	<div>
 		Tabs
+		<component :is="defaults[0]"/>
+		<component :is="defaults[1]"/>
 	</div>
 </template>
 
 <script lang="ts">
+import Tab from "./Tab.vue";
+
 export default {
-	name: "Tabs"
+	name: "Tabs",
+	setup(props, context) {
+		console.log({...context.slots.default()})
+
+		const defaults = context.slots.default();
+		defaults.forEach(tag => {
+			if(tag.type !== Tab){
+				throw new Error('子组件不是Tab');
+			}
+		})
+		return {
+			defaults
+		}
+	}
 };
 </script>
 
