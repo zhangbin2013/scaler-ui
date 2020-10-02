1b7018e6ebe8e694b885a6a30f8b00535a9b3d66
zhangbin_yq <zhangbin_yq@kedacom.com>
2020-10-02 10:05:11 +0800

    Dialog 组件样式+切换

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,31 @@
 <template>
 	<section>
-		<Dialog/>
+		<div>Dialog 示例</div>
+		<h1>示例1</h1>
+		<Button @click="toggle">toggle</Button>
+		<Dialog :visible="x"/>
 	</section>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from "../lib/Button.vue";
+import {ref} from 'vue';
 
 export default {
 	name: 'DialogDemo',
-	components: {Dialog}
+	components: {Button, Dialog},
+	setup() {
+		const x = ref(false);
+
+		const toggle = () => {
+			x.value = !x.value;
+		}
+		return {
+			x,
+			toggle
+		}
+	}
 };
 </script>
 
