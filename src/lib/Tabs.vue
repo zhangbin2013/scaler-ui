99f5dd53c97c0d56d52823552065efca5e010a32
zhangbin_yq <zhangbin_yq@kedacom.com>
2020-10-02 15:06:50 +0800

    tabs css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,12 @@
 <template>
-	<div>
-		<div v-for="(title,index) in titles">{{title}}</div>
-		<component v-for="(c,index) in defaults" :is="c" :key="index"/>
+	<div class="sc-tabs">
+		<div class="sc-tabs-nav">
+			<div class="sc-tabs-nav-item" v-for="(title,index) in titles">{{ title }}</div>
+		</div>
+		<div class="sc-tabs-content">
+			<component class="sc-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+		</div>
+
 	</div>
 </template>
 
@@ -15,7 +20,7 @@ export default {
 
 		const defaults = context.slots.default();
 		defaults.forEach(tag => {
-			if(tag.type !== Tab){
+			if (tag.type !== Tab) {
 				throw new Error('子组件不是Tab');
 			}
 		})
@@ -29,5 +34,32 @@ export default {
 </script>
 
 <style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.sc-tabs {
+	&-nav {
+		display: flex;
+		color: $color;
+		border-bottom: 1px solid $border-color;
 
+		&-item {
+			padding: 8px 0;
+			margin: 0 16px;
+			cursor: pointer;
+
+			&:first-child {
+				margin-left: 0;
+			}
+
+			&.selected {
+				color: $blue;
+			}
+		}
+	}
+
+	&-content {
+		padding: 8px 0;
+	}
+}
 </style>
