5c096b301b70c7fd2e9af3a1f5b4b3b394afdb5c
zhangbin_yq <zhangbin_yq@kedacom.com>
2020-10-26 10:22:05 +0800

    更换icon

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ import {createApp} from 'vue';
 import App from './App.vue';
 
 import {router} from "./router";
-// import './assets/svg.js'
-import '//at.alicdn.com/t/font_2154058_byikmgz44tm.js';
+import './assets/svg.js'
 
 import 'github-markdown-css';
 
